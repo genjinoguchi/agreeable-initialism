3ce55bed83eaf47c88de1814a521f6c00cbeb473
Michael <merickson@Millenniumbrooklynhs.org>
2015-02-07 11:23:58 -0500

    Added: Tic Tac Toe.py File

---
+++ tic tac toe.py
@@ -0,0 +1,15 @@
+space = " "
+board = '''
+   |   |   
+ %s | %s | %s 
+___|___|___
+   |   |   
+ %s | %s | %s 
+___|___|___
+   |   |   
+ %s | %s | %s 
+   |   |   
+'''
+
+print board%("x", space, space, space, space, space, space, space, space)
+
